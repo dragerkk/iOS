43532d8630e11ac56184775cf2bc9cd1f76bda17
kang-juwon <dragerkk@gmail.com>
2022-04-27 15:43:53 +0900

    algo num+1010ing

--- Algorithm/Baekjun/13_Number&Combinatoric/13_Number&Combinatorics/13_Number&Combinatorics/main.swift
+++ Algorithm/Baekjun/13_Number&Combinatoric/13_Number&Combinatorics/13_Number&Combinatorics/main.swift
@@ -215,19 +215,51 @@ import Foundation
 //}
 
 
-// 11051 이항계수2 -1, 런타임에러
-// 정확한 원인은 모르지만 N이 1000까지 올 수 있기 때문에 수가 너무 커져서 오버플로우 발생하는 듯함. 팩토리얼을 사용한 방법으로는 무리인 것으로 보임
+//// 11051 이항계수2 -1, factorial 이용, 런타임에러
+//// 정확한 원인은 모르지만 N이 1000까지 올 수 있기 때문에 수가 너무 커져서 오버플로우 발생하는 듯함. 팩토리얼을 사용한 방법으로는 무리인 것으로 보임
+//
+//let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+//
+//var result = getFactorial(input[0]) / (getFactorial(input[1])*getFactorial(input[0]-input[1]))
+//
+//print(result%10007)
+//
+//func getFactorial (_ num:Int) -> Int {
+//	if num <= 1 {
+//		return 1
+//	}
+//	return num * getFactorial(num-1)
+//}
 
-let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+//
+//// 11051 이항계수2 -2, 파스칼의 삼각형 이용
+//
+//let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+//var arr: [[Int]] = Array(repeating: Array(repeating: 0, count: input[0]+1), count: input[0]+1)
+//var result = 0
+//
+//for i in 1...input[0] {
+//	for j in 0...input[0] {
+//		if j == 0 || j == i {
+//			arr[i][j] = 1
+//		} else {
+//			arr[i][j] = (arr[i-1][j-1] + arr[i-1][j])%10007 // 10007로 나눈 나머지 값을 배열에 넣어야 오버플로우가 발생하지 않는 듯.
+//		}
+//	}
+//}
+//
+//print(arr[input[0]][input[1]])
 
-var result = getFactorial(input[0]) / (getFactorial(input[1])*getFactorial(input[0]-input[1]))
 
-print(result%10007)
+// 1010 다리놓기
 
-func getFactorial (_ num:Int) -> Int {
-	if num <= 1 {
-		return 1
-	}
-	return num * getFactorial(num-1)
+let caseNum = Int(readLine()!)!
+
+for _ in 1...caseNum {
+	let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+	let result = 0
+	let siteA: [Int] = Array(1...input[0])
+	let siteB: [Int] = Array(1...input[1])
+	
+	
 }
-
