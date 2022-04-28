586e3897d913ba2e9fcf6130e50e1a9431909289
kang-juwon <dragerkk@gmail.com>
2022-04-28 19:32:01 +0900

    tipsy tried mvc but nearly nothing to modify

---
+++ Project&Practice/Bootcamp/12_Tipsy/Tipsy/Model/CalculateTip.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Tipsy
+//
+//  Created by 강주원 on 2022/04/28.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+class CalculateTip {
+	
+	var numPeople: Double = 0
+	var totalAmount: Double = 0
+	var tipPercent: Double = 0
+	
+	// 딱히 mvc로 할 게 없는듯..?
+	
+}
