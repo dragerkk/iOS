9054db56ca137448a7c12fbdc5a99ccdae759427
kang-juwon <dragerkk@gmail.com>
2022-05-01 18:56:40 +0900

    proj clima +JSON decoding

--- Project&Practice/Bootcamp/13_Clima/Clima/WeatherManager.swift
+++ Project&Practice/Bootcamp/13_Clima/Clima/WeatherManager.swift
@@ -25,22 +25,48 @@ struct WeatherManager {
 			let session = URLSession(configuration: .default)
 			
 			// 3. Give the session a task
-			let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
-			
+			// let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+			let task = session.dataTask(with: url) { (data, response, error) in
+				if error != nil {
+					print(error!)
+					return // exit out of this function.
+				}
+				
+				if let safeData = data {
+					parseJSON(weatherData: safeData)
+				}
+			}
 			// 4. Start the task
 			task.resume()
 		}
 	}
 	
-	func handle(data: Data?, response: URLResponse?, error: Error?) {
-		if error != nil {
-			print(error!)
-			return // exit out of this function.
-		}
-		
-		if let safeData = data {
-			let dataString = String(data: safeData, encoding: .utf8) // utf8 : 웹사이트에서 encoding 표준규격인듯?
-			print(dataString)
+	
+	//func handle(data: Data?, response: URLResponse?, error: Error?) {
+	//	if error != nil {
+	//		print(error!)
+	//		return // exit out of this function.
+	//	}
+	//
+	//	if let safeData = data {
+	//		let dataString = String(data: safeData, encoding: .utf8) // utf8 : 웹사이트에서 encoding 표준규격인듯?
+	//		print(dataString)
+	//	}
+	//}
+	
+	
+	
+	// -------JSON
+	
+	func parseJSON(weatherData: Data) {
+		let decoder = JSONDecoder()
+		do {
+			let decodedData = try decoder.decode(WeatherData.self, from: weatherData )
+			print(decodedData.name)
+			print(decodedData.main.temp)
+			print(decodedData.weather[0].description)
+		} catch {
+			print(error)
 		}
 	}
 }
