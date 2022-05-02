ecf3fa65e116971627357561c5ee6c014d6fe92a
kang-juwon <dragerkk@gmail.com>
2022-05-02 17:50:35 +0900

    algo prac

--- Project&Practice/practice/practice/main.swift
+++ Project&Practice/practice/practice/main.swift
@@ -1,46 +1,19 @@
 import Foundation
+// 연산자 우선순위
 
-var arr1 = [9,20,28,18,11]
-var arr2 = [30,1,21,17,28]
-let n = 5
+let expression = "100-200*300-500+20" // answer:60420
 
-var answer = Array(repeating: "#", count: n)
 
-var binary1 = Array(repeating: 0, count: n)
-var binary2 = Array(repeating: 0, count: n)
+let x = 12
+var num = x
+var sum = 0
 
-var combineArr = Array(repeating: "0", count: n)
-
-var binArr = [String]()
-// 1. 이진수로 바꿈 2. 이진수 0->" ", 1->"#"바꿈 3. 배열비교해서 하나라도 #이면 #을 새배열에 넣음 4.캐릭터를 스트링으로 바꿔줌
-for i in 0..<n {
-	while arr1[i] >= 2 && arr2[i] >= 2 {
-		for j in (0..<n).reversed() {
-			let res1 = arr1[i] % 2
-			let res2 = arr2[i] % 2
-			binary1[j] = res1
-			binary2[j] = res2
-			arr1[i] /= 2
-			arr2[i] /= 2
-		}
-	}
-	let stringBin1 = binary1.map{String($0)}
-	let stringBin2 = binary2.map{String($0)}
-	print(stringBin1)
-	print(stringBin2)
-	
-	for i in 0..<n {
-		if stringBin1[i] == "1" || stringBin2[i] == "1" {
-			combineArr[i] = "#"
-		} else {
-			combineArr[i] = " "
-		}
-	}
-	print("c: \(combineArr)")
-	binArr.append(combineArr.joined(separator: ""))
+while num >= 10 {
+	sum += num % 10
+	num /= 10
 }
-
-print(binArr)
-
-
-
+if x % sum == 0 {
+	print(true)
+} else {
+	print(false)
+}
