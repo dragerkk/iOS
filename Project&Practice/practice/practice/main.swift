eac022ae1667023c6ad72ff1304f3f6b22d3f73c
kang-juwon <dragerkk@gmail.com>
2022-05-05 19:59:36 +0900

    algorithm practice

--- Project&Practice/practice/practice/main.swift
+++ Project&Practice/practice/practice/main.swift
@@ -3,36 +3,21 @@ import Foundation
 
 let expression = "100-200*300-500+20" // answer:60420
 
+let s = "qwer"
 
-let s = "a B z"
-let n = 4
-
-var str = Array(s)
-let lower : [Character] = Array("abcdefghijklmnopqrstuvwxyz")
-let upper : [Character] = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-for i in 0..<str.count {
-	if str[i] == " " {
-		continue
-	}
-	if lower.contains(str[i]) {
-		let index = lower.firstIndex(of: str[i])!
-		if index + n > 25 {
-			str[i] = lower[index+n-26]
-		} else {
-			str[i] = lower[index+n]
-		}
-	}
-	if upper.contains(str[i]) {
-		let index = upper.firstIndex(of: str[i])!
-		if index + n > 25 {
-			str[i] = upper[index+n-26]
-		} else {
-			str[i] = upper[index+n]
-		}
-	}
-
+print(s.count)
+if Array(s).count % 2 == 0 {
+	print(String(Array(s)[s.count/2-1...s.count/2]))
+} else {
+	print(String(Array(s)[s.count/2]))
 }
-
-print(lower.count)
-print(String(str))
+//
+//func solution(_ s:String) {
+//	if Array(s).count % 2 == 0 {
+//		print(String(Array(s)[(s.count/2)-1...(s.count/2)]))
+//	}else{
+//		print(String(Array(s)[s.count/2]))
+//	}
+//}
+//
+//solution("qwer")
