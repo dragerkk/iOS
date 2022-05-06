c10b0a5ba2eb4ff2cd4470a6d243fb90c57a500d
kang-juwon <dragerkk@gmail.com>
2022-05-06 19:42:44 +0900

    algo +prog

--- Project&Practice/practice/practice/main.swift
+++ Project&Practice/practice/practice/main.swift
@@ -3,21 +3,52 @@ import Foundation
 
 let expression = "100-200*300-500+20" // answer:60420
 
-let s = "qwer"
+//let arr = expression.components(separatedBy: CharacterSet(charactersIn: "-+*"))
 
-print(s.count)
-if Array(s).count % 2 == 0 {
-	print(String(Array(s)[s.count/2-1...s.count/2]))
-} else {
-	print(String(Array(s)[s.count/2]))
+
+
+let N = 5
+let stages = [2,1,4,3,5]
+
+var userLevel = stages.sorted()
+var stageCleared = Array(repeating: 0, count: N+1)
+
+for i in 0..<userLevel.count {
+	if userLevel[i] != 1 {
+		for j in 1..<userLevel[i] {
+			stageCleared[j] += 1
+		}
+	}
 }
-//
-//func solution(_ s:String) {
-//	if Array(s).count % 2 == 0 {
-//		print(String(Array(s)[(s.count/2)-1...(s.count/2)]))
-//	}else{
-//		print(String(Array(s)[s.count/2]))
-//	}
-//}
-//
-//solution("qwer")
+
+var dict : [Int:Int] = [:]
+for i in 0...N {
+	dict[i] = stageCleared[i]
+}
+
+var failRate : [Int:Double] = [:] // stage : failRate
+stageCleared[0] = stages.count
+
+for i in 1...N {
+	if stageCleared[i-1] == 0 {
+		failRate[i] = 0
+	} else {
+		failRate[i] = (1 - (Double(stageCleared[i])) / Double(stageCleared[i-1]))
+	}
+}
+
+let result = failRate.sorted {
+	if $0.1 == $1.1 {
+		return $0 < $1
+	} else {
+		return $0.1 > $1.1
+	}
+}
+
+var returnArr : [Int] = []
+
+for i in 0..<result.count {
+	returnArr.append(result[i].key)
+}
+
+print(returnArr)
