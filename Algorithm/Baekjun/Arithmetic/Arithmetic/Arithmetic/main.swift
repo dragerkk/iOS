20dc1eddc48e12c8744d29b0320c44951b0b58bb
kang-juwon <dragerkk@gmail.com>
2022-05-28 23:27:38 +0900

    algo arithmetic 2480

--- Algorithm/Baekjun/Arithmetic/Arithmetic/Arithmetic/main.swift
+++ Algorithm/Baekjun/Arithmetic/Arithmetic/Arithmetic/main.swift
@@ -1,3 +1,30 @@
 // 2480 - 주사위 세 개
 
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 
+var dice = [1:0, 2:0, 3:0, 4:0, 5:0, 6:0]
+
+for i in input {
+	dice[i]! += 1
+}
+
+var sum = 0
+var one = [Int]()
+
+for i in 1...6 {
+	if dice[i] == 3 {
+		sum = 10000 + i * 1000
+		break
+	} else if dice[i] == 2 {
+		sum = 1000 + i * 100
+		break
+	} else if dice[i] == 1 {
+		one.append(i)
+	}
+}
+
+if sum != 0 {
+	print(sum)
+} else {
+	print(one.max()!*100)
+}
