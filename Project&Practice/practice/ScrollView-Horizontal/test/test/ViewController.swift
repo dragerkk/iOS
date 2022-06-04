e0fc0bdd656cdf0760c25364e66bd8c87fd7f349
kang-juwon <dragerkk@gmail.com>
2022-06-05 01:23:18 +0900

    test with Kingfisher

--- Project&Practice/practice/ScrollView-Horizontal/test/test/ViewController.swift
+++ Project&Practice/practice/ScrollView-Horizontal/test/test/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var image1: UIImageView!
+	
+	let url = URL(string:"https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201812022340")
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		image1.kf.setImage(with: url)
+		
 		// Do any additional setup after loading the view.
+		
 	}
 
 
