37aa44c4e142229d54cd93a2b3c84315d0098279
kang-juwon <dragerkk@gmail.com>
2022-06-05 17:09:43 +0900

    readme add & test dir change

---
+++ Project&Practice/README.md
@@ -0,0 +1,11 @@
+# practice with projects. 
+
+
+## practice
+### test specific function
+- horizontal scroll view with stackview
+- button design changed when scroll up
+- scroll view pinned when scroll down
+
+### include 
+- scroll view, navigation bar, tab bar
--- Project&Practice/practice/test/test/ViewController.swift
+++ Project&Practice/practice/test/test/ViewController.swift
@@ -12,14 +12,20 @@ class ViewController: UIViewController {
 
 	@IBOutlet weak var button1: UIButton!
 	@IBOutlet weak var image1: UIImageView!
+	@IBOutlet weak var scrollView1: UIScrollView!
 	
 	let url = URL(string:"https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201812022340")
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		image1.kf.setImage(with: url)
-		
 		// Do any additional setup after loading the view.
+	}
+	
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		if scrollView.isDragging {
+			button1.backgroundColor = .blue
+		}
 	}
 
 }
