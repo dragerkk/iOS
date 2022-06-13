9759b6fd31019af2ba2c972dac8e62d05ba88e5d
kang-juwon <dragerkk@gmail.com>
2022-06-13 23:36:30 +0900

    rs clone2 rename & memo begin

---
+++ Project&Practice/RisingCamp/3_Memo/3_Memo/FolderManage/FolderManageVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3_Memo
+//
+//  Created by 강주원 on 2022/06/13.
+//
+
+import UIKit
+
+class FolderManageVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
