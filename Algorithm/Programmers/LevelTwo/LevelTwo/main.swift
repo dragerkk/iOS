89dfe14ee1276e59ef3af595ef81fd1e42e92db5
kang-juwon <dragerkk@gmail.com>
2022-06-23 16:47:00 +0900

    algo prog lev2 +

--- Algorithm/Programmers/LevelTwo/LevelTwo/main.swift
+++ Algorithm/Programmers/LevelTwo/LevelTwo/main.swift
@@ -16,3 +16,32 @@
 //}
 
 
+// 스킬트리
+
+import Foundation
+
+func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+	
+	var result = 0
+
+	for i in skill_trees {
+		var s = skill
+		var check = true
+		for j in i {
+			if s.contains(j) && s.first != j {
+				check = false
+				break
+			} else if j == s.first {
+				check = true
+				s.removeFirst()
+			}
+			if s.count == 0 {
+				break
+			}
+		}
+		if check != false {
+			result += 1
+		}
+	}
+	return result
+}
