011a61413390503b38b82c531ff9779307b7a1ae
kang-juwon <dragerkk@gmail.com>
2022-06-25 03:50:03 +0900

    bst summarized fin

--- Algorithm/BST/BST/main.swift
+++ Algorithm/BST/BST/main.swift
@@ -32,26 +32,90 @@ class BST <T: Comparable> {
 		print("No \(data) in BST")
 	}
 	
+	
 	func insert(_ data: T) {
 		if rootNode == nil {
-			rootNode = Node(data: data)
+			rootNode = Node.init(data: data)
 			return
 		}
 		
 		var currentNode = rootNode
 		
 		while let node = currentNode {
-			if data < node.data && node.leftChild == nil {
-				node.leftChild = Node(data: data)
-			} else if data < node.data && node.leftChild != nil {
-				currentNode = node.leftChild
-			} else if data > node.data && node.rightChild == nil {
-				node.rightChild = Node(data: data)
-			} else {
-				currentNode = node.rightChild
+			
+			if data < node.data {
+				guard let nextNode = node.leftChild else {
+					return node.leftChild = Node.init(data: data)
+				}
+				currentNode = nextNode
+			} else { // 중복된 노드가 삽입되지는 않을 것이므로 data > node.data를 그냥 else로 처리
+				guard let nextNode = node.rightChild else {
+					return node.rightChild = Node.init(data: data)
+				}
+				currentNode = nextNode
 			}
 		}
+	
 	}
+	
+	
+	func remove(_ data: T) {
+		
+		guard let rootNode = rootNode else { return }
+		
+		var parentNode:Node = rootNode
+		var currentNode:Node? = rootNode // 아래의 while문을 위해 optional로 줌
+		
+		while let node = currentNode {
+			
+			if node.data == data {
+				if node.leftChild != nil && node.rightChild != nil { //자식이 2개 있을 때
+					var findNodes = findSuccessorNode(node: node) //후속자노드 찾기
+					var parentSuccessorNode = findNodes[0] //후속자노드의 부모노드
+					var successorNode = findNodes[1] //후속자노드
+					
+					if successorNode.rightChild != nil { // 후속자노드에 오른쪽 자식이 존재할 경우
+						currentNode?.data = successorNode.data
+						successorNode.data = successorNode.rightChild!.data
+						successorNode.rightChild = nil
+					} else {
+						currentNode?.data = successorNode.data
+						parentSuccessorNode.leftChild = nil
+					}
+				} else if node.leftChild != nil { // 왼쪽 자식만 있을 때
+					currentNode?.data = node.leftChild!.data
+					currentNode?.leftChild = nil
+				} else if node.rightChild != nil { // 오른쪽 자식만 있을 때
+					currentNode?.data = node.rightChild!.data
+					currentNode?.rightChild = nil
+				} else { // 자식이 없을때
+					if node.data < parentNode.leftChild!.data {
+						parentNode.leftChild = nil
+					} else {
+						parentNode.rightChild = nil
+					}
+				}
+				break // node.data == data 일때 위의 코드들 실행 후 break.
+			}
+			
+			parentNode = currentNode ?? parentNode
+			currentNode = data < node.data ? node.leftChild : node.rightChild
+		}
+		
+	}
+	
+	func findSuccessorNode(node:Node<T>) -> [Node<T>] {
+		var parentNode: Node<T> = node
+		var successorNode: Node<T>?  = node.rightChild
+		
+		while successorNode?.leftChild != nil {
+			parentNode = successorNode!
+			successorNode = successorNode?.leftChild!
+		}
+	
+		return [parentNode, successorNode!]
+	}
+	
 	
 // MARK: - draw Diagram
 	func drawDiagram() {
@@ -74,16 +138,21 @@ class BST <T: Comparable> {
 	}
 }
 
-let bst = BST<Int>()
+var bst = BST<Int>()
 bst.insert(20)
 bst.insert(15)
 bst.insert(30)
 bst.insert(12)
 bst.insert(16)
-
+bst.insert(27)
+bst.insert(35)
+bst.insert(29)
+bst.insert(32)
+bst.insert(38)
 bst.drawDiagram()
 
-bst.search(22)
+bst.remove(30)
+bst.drawDiagram()
 //bst.search(from: 22)
 
 
