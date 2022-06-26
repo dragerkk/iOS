ea2915a3523037917abe3502cae14a991d54ea34
kang-juwon <dragerkk@gmail.com>
2022-06-27 01:11:35 +0900

    algo prog lev2 +

--- Algorithm/Programmers/LevelTwo/LevelTwo/main.swift
+++ Algorithm/Programmers/LevelTwo/LevelTwo/main.swift
@@ -16,32 +16,56 @@
 //}
 
 
-// 스킬트리
+//// 스킬트리
+//
+//import Foundation
+//
+//func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+//
+//	var result = 0
+//
+//	for i in skill_trees {
+//		var s = skill
+//		var check = true
+//		for j in i {
+//			if s.contains(j) && s.first != j {
+//				check = false
+//				break
+//			} else if j == s.first {
+//				check = true
+//				s.removeFirst()
+//			}
+//			if s.count == 0 {
+//				break
+//			}
+//		}
+//		if check != false {
+//			result += 1
+//		}
+//	}
+//	return result
+//}
 
-import Foundation
 
-func solution(_ skill:String, _ skill_trees:[String]) -> Int {
-	
-	var result = 0
+//// N개의 최소공배수
+//
+//func solution(_ arr:[Int]) -> Int {
+//
+//	return arr.reduce(1) { lcm($0, $1) }
+//}
+//
+//func gcd(_ n1:Int, _ n2:Int) -> Int {
+//	let r = n1 % n2
+//	if r != 0 {
+//		return gcd(n2, r)
+//	} else {
+//		return n2
+//	}
+//}
+//
+//func lcm(_ n1:Int, _ n2:Int) -> Int {
+//
+//	return (n1 * n2) / gcd(n1,n2)
+//}
+//
 
-	for i in skill_trees {
-		var s = skill
-		var check = true
-		for j in i {
-			if s.contains(j) && s.first != j {
-				check = false
-				break
-			} else if j == s.first {
-				check = true
-				s.removeFirst()
-			}
-			if s.count == 0 {
-				break
-			}
-		}
-		if check != false {
-			result += 1
-		}
-	}
-	return result
-}
