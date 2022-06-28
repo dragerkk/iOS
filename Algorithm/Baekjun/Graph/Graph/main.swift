452904ff83663730c0c85478711ed87a593e843c
kang-juwon <dragerkk@gmail.com>
2022-06-28 23:40:07 +0900

    proj 5th start

--- Algorithm/Baekjun/Graph/Graph/main.swift
+++ Algorithm/Baekjun/Graph/Graph/main.swift
@@ -1,11 +1,38 @@
-//
-//  main.swift
-//  Graph
-//
-//  Created by 강주원 on 2022/06/27.
-//
 
+// 24479 깊이 우선 탐색 1
 import Foundation
 
-print("Hello, World!")
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+var dict = [Int:[Int]]()
+
+for _ in 1...input[0] {
+	let n = readLine()!.split(separator: " ").map{Int(String($0))!}
+	if dict[n[0]] == nil {
+		dict[n[0]] = [n[1]]
+	} else if !dict[n[0]]!.contains(n[1]) {
+		dict[n[0]]?.append(n[1])
+	}
+
+	if dict[n[1]] == nil {
+		dict[n[1]] = [n[0]]
+	} else if !dict[n[1]]!.contains(n[0]) {
+		dict[n[1]]?.append(n[0])
+	}
+}
+
+for i in 0..<dict.count {
+	dict[i]?.sort()
+}
+
+print(dict)
+
+var start = input[2]
+
+depthFirstSearch(graph: dict, vertex: start)
+
+
+func depthFirstSearch(graph: [Int:[Int]], vertex: Int, check:[Int:Bool]=[:]) {
+	
+}
 
---
+++ Project&Practice/StudyWithProject/5_LibraryPractice/5_LibraryPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5_LibraryPractice
+//
+//  Created by 강주원 on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
