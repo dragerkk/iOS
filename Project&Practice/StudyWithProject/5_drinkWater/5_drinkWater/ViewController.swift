62c51211a2d5b337b3382d0daa45f97d12c11f78
kang-juwon <dragerkk@gmail.com>
2022-07-03 23:17:07 +0900

    5th proj with snapkit

--- Project&Practice/StudyWithProject/5_drinkWater/5_drinkWater/ViewController.swift
+++ Project&Practice/StudyWithProject/5_drinkWater/5_drinkWater/ViewController.swift
@@ -6,13 +6,27 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+	var sampleView: UIView = {
+		let view = UIView()
+		view.backgroundColor = .systemBlue
+		return view
+	} ()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		view.backgroundColor = .white
+		
+		self.view.addSubview(sampleView)
+		sampleView.snp.makeConstraints{ (make) in
+			make.edges.equalTo(view).inset(UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
+		}
 	}
+	
 
 
 }
