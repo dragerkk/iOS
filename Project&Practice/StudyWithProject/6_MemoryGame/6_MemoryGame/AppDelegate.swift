a543a184e23f7cb78444e83c30cd1a538cb16bb2
kang-juwon <dragerkk@gmail.com>
2022-07-07 03:08:50 +0900

    proj 6th set orientation

--- Project&Practice/StudyWithProject/6_MemoryGame/6_MemoryGame/AppDelegate.swift
+++ Project&Practice/StudyWithProject/6_MemoryGame/6_MemoryGame/AppDelegate.swift
@@ -10,8 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+	var orientationLock = UIInterfaceOrientationMask.portrait
 
+	// MARK: - Set orientation : Portrait
+	func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+		return self.orientationLock
+	}
 
+	// ------
+	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		return true
